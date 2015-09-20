52e39ba300795bba2313dce89ca39198357a8fbd
Adrian <pheh.adrian@gmail.com>
2015-09-21 01:26:28 +0800

    added getCommandType stub for Parser

--- src/todo/Parser.java
+++ src/todo/Parser.java
@@ -1,6 +1,11 @@
 package todo;
 
+import struct.*;
+
 public class Parser {
 
-
+    public Input getCommandType(String inputText) {
+        // Function stub, emulating a Task
+        return new Input(new Task());
+    }
 }
