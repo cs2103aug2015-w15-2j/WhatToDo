5a0e1e6d627b71df7bf8d7853e815d7c4efef09b
Adrian <pheh.adrian@gmail.com>
2015-09-21 01:13:36 +0800

    added Input class for Parser to return

--- src/struct/Input.java
+++ src/struct/Input.java
@@ -1,17 +1,49 @@
 package struct;
 
-import struct.Date;
-
 public class Input {
 
+    boolean isTask;
+    Task task;
+    Event event;
 
+    public Input(Task task) {
+        isTask = true;
+        this.task = task;
+        this.event = null;
+    }
 
+    public Input(Event event) {
+        isTask = false;
+        this.event = event;
+        this.task = null;
+    }
 
+    // ------------------------Getters------------------------
+    public Task getTask() {
+        return task;
+    }
 
-    public Input() {
+    public Event getEvent() {
+        return event;
+    }
 
+    // ------------------------Setters------------------------
+    /* These work on the assumption that an Input can only be
+     * either a Task or an Event, but not both. Either way,
+     * there is no real requirement for these setters because
+     * Input objects will only instantiated with the Tasks or
+     * Events
+     */
 
-        // Initialize event variables
-        eventStartDate
+    public void setTask(Task task) {
+        isTask = true;
+        this.task = task;
+        this.event = null;
+    }
+
+    public void setEvent(Event event) {
+        isTask = false;
+        this.event = event;
+        this.task = null;
     }
 }
