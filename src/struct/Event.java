e9cec1401dd1e044375b1940bc677c109a5da536
garbanzos <chrno_crusade217@hotmail.com>
2015-10-06 14:23:18 +0800

    added constructors to make task or event obj based on line of text stored in the txt file

--- src/struct/Event.java
+++ src/struct/Event.java
@@ -6,8 +6,10 @@
 
 package struct;
 
-public class Event extends Data{
+public class Event extends Data implements Comparable<Event>{
 
+	private static final String SEMICOLON = ";";
+	
     private Date eventStartDate, eventEndDate;
     private String eventStartTime, eventEndTime;
     
@@ -22,6 +24,22 @@ public class Event extends Data{
         this.eventStartTime = "";
         this.eventEndTime = "";
     }
+    
+    public Event(String line){ 
+    	line.trim();
+    	String[] lineComponents = line.split(SEMICOLON);
+    	
+    	this.name = lineComponents[1]; 
+    	this.isDone = false; 
+
+    	//assert correct no. of components 
+    	//assert first word is event
+    	this.eventStartDate = new Date(lineComponents[2]);
+    	this.eventEndDate = new Date(lineComponents[4]);
+    	this.eventStartTime = lineComponents[3];
+    	this.eventEndTime = lineComponents[5];
+    	
+    }
 
 	public Event(String name, boolean isDone, Date eventStartDate, Date eventEndDate, String eventStartTime, String eventEndTime) {
 		super(name, isDone);
@@ -29,6 +47,19 @@ public class Event extends Data{
 		this.eventEndDate = eventEndDate;
 		this.eventStartTime = eventStartTime;
 		this.eventEndTime = eventEndTime;
+	}
+	
+	//TODO compareto 
+	public int compareTo(Event other){ 
+		Date thisStartDate = this.getEventStartDate(); 
+		
+		Date otherStartDate = other.getEventStartDate(); 
+	
+		if(thisStartDate.compareTo(otherStartDate) == -1){ 
+			return -1;
+		}
+		
+		return 0; 
 	}
 
 	// ------------------------Getters------------------------
--- src/struct/Task.java
+++ src/struct/Task.java
@@ -6,8 +6,12 @@
 
 package struct;
 
-public class Task extends Data{
+public class Task extends Data implements Comparable<Task>{
 
+	private static final String SEMICOLON = ";";
+	private static final String TYPE_FLOAT = "float";
+	private static final String TYPE_TASK = "task";
+	
     private Date deadline;
     private boolean isFloating;
     
@@ -20,11 +24,38 @@ public class Task extends Data{
         this.deadline = new Date();
         this.isFloating = true;
     }
+    
+    public Task(String line) {
+    	line.trim();
+    	String[] lineComponents = line.split(SEMICOLON);
+    	
+    	this.name = lineComponents[1]; 
+    	this.isDone = false; 
+    	
+    	//assert correct no. of components 2 or 3
+    	//assert first word is float or task 
+    	if(lineComponents[0].equals(TYPE_FLOAT)){
+    		this.isFloating = true; 
+    		this.deadline = null; 
+    	}
+    	else{
+    		this.isFloating = false; 
+    		this.deadline = new Date(lineComponents[2]);
+    	}
+    }
    
 	public Task(String name, boolean isDone, Date deadline, boolean isFloating) {
 		super(name, isDone);
 		this.deadline = deadline;
 		this.isFloating = isFloating;
+	}
+	
+	//============================================
+	// Public Methods
+	//============================================
+	//TODO compareTo
+	public int compareTo(Task other){
+		return 0; 
 	}
 
 	//============================================
