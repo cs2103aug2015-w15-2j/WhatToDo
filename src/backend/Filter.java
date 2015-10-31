aa538683ad602292a5fe16575bee036cffed67f2
garbanzos <chrno_crusade217@hotmail.com>
2015-11-01 02:37:24 +0800

    can now show unresolved Events

--- src/backend/Filter.java
+++ src/backend/Filter.java
@@ -2,27 +2,30 @@ package backend;
 
 import java.util.ArrayList;
 
+import struct.Date;
+
 public class Filter {
 	
 	private static final int INDEX_TYPE = 0; 
 	private static final int INDEX_NAME = 1; 
 	private static final int INDEX_ISDONE = 2; 
 	private static final int INDEX_DUEDATE = 3;
-	private static final int INDEX_STARTDATE = 3; 
-	private static final int INDEX_STARTTIME = 4; 
 	private static final int INDEX_ENDDATE = 5; 
-	private static final int INDEX_ENDTIME = 6;
+	
+//    private static final String TYPE_FLOAT = "float";
+    private static final String TYPE_TASK = "task";
+//    private static final String TYPE_EVENT = "event";
 	
 	private static final String DONE = "done"; 
 	
 	private static final String REGEX_WHITESPACE = "\\s+"; 
 	
 	private static final String SEMICOLON = ";";
-
-	public Filter(){
-		
-	}
 	
+	//============================================
+	// Public methods
+	//============================================
+		
 	public ArrayList<Integer> filterStatus(String[] linesInFile, String type, boolean isDone){ 
 		 ArrayList<Integer> resultList = new ArrayList<Integer>(); 
 		 
@@ -34,6 +37,54 @@ public class Filter {
 		 }
 		 
 		 return resultList; 
+	}
+	
+	public ArrayList<Integer> filterPastUncompleted(String[] linesInFile, String type){ 
+		//assert type is not float 
+		ArrayList<Integer> resultList = new ArrayList<Integer>(); 
+		
+		for(int i = 0; i < linesInFile.length; i++){ 
+			 String line = linesInFile[i];
+			 if(isType(type, line) && isPast(type, line) &&isReqStatus(false, line)){
+				 resultList.add(i); 
+			 }
+		 }
+
+		return resultList; 
+	}
+		
+	public ArrayList<Integer> matchTokensInQuery(String[] linesInFile, String type, String query){ 
+		
+		String[] tokens = parseQuery(query); 
+		ArrayList<Integer> resultList = new ArrayList<Integer>(); 
+		
+		for(int i = 0; i < linesInFile.length; i++){ 
+			String line = linesInFile[i];
+			if(isType(type, line) && containToken(tokens, line)){
+				resultList.add(i); 
+			}
+		}
+		
+		return resultList; 
+	}
+	
+	//TODO match query with quotes 
+	public String matchQuery(String query){
+		
+		//remove trailing quotes at the start and end 
+		//use .contains
+		
+		return "stringmatches"; 
+	}
+	
+	//============================================
+	// Private methods 
+	//============================================
+	
+	private boolean isType(String type, String line){
+		String[] lineFields = line.split(SEMICOLON);
+		String lineType = lineFields[INDEX_TYPE];
+		return lineType.equals(type); 
 	}
 	
 	private boolean isReqStatus(boolean isDone, String line){ 
@@ -51,19 +102,19 @@ public class Filter {
 		return lineIsDone.equals(DONE); 
 	}
 	
-	public ArrayList<Integer> matchTokensInQuery(String[] linesInFile, String type, String query){ 
+	private boolean isPast(String type, String line){ 
+		//assert type is not float 
+		String[] lineFields = line.split(SEMICOLON);
+		String lineDateStr = (type.equals(TYPE_TASK)) ? lineFields[INDEX_DUEDATE] : lineFields[INDEX_ENDDATE]; 
+		Date lineDate = new Date(lineDateStr); 
+		Date todayDate = Date.todayDate(); 
 		
-		String[] tokens = parseQuery(query); 
-		ArrayList<Integer> resultList = new ArrayList<Integer>(); 
-		
-		for(int i = 0; i < linesInFile.length; i++){ 
-			String line = linesInFile[i];
-			if(isType(type, line) && containToken(tokens, line)){
-				resultList.add(i); 
-			}
+		if(lineDate.compareTo(todayDate) < 0){
+			return true; 
 		}
-		
-		return resultList; 
+		else{ 
+			return false; 
+		}
 	}
 	
 	private String[] parseQuery(String rawQuery){ 
@@ -85,21 +136,4 @@ public class Filter {
 		
 		return result; 
 	}
-	
-	private boolean isType(String type, String line){
-		String[] lineFields = line.split(SEMICOLON);
-		String lineType = lineFields[INDEX_TYPE];
-		return lineType.equals(type); 
-	}
-	
-	//TODO match query with quotes 
-	public String matchQuery(String query){
-		
-		//remove trailing quotes at the start and end 
-		//use .contains
-		
-		return "stringmatches"; 
-	}
-	
-	//TODO filter for views 
 }
