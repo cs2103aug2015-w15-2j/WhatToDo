776a39df2129dac67e56576aa09e8a32e2864cd0
Thoughtss <lk1908@hotmail.com>
2015-11-09 20:48:16 +0800

    final refactoring for parser class done

--- src/backend/TimeParser.java
+++ src/backend/TimeParser.java
@@ -1,90 +1,63 @@
+/**
+ * This class parses user input string for time.
+ * 
+ * @@author A0124099B
+ */
+
 package backend;
 
-//@@author A0124099B
 public class TimeParser {
+	
+	private static final int POSITION_DIFFERENCE_TWO = 2;
 
-	private static final String REGEX_12_HOUR_SIMPLE_TIME = "(1[012]|[1-9]|0[1-9])(\\s)?(?i)(am|pm)";
-	private static final String REGEX_12_HOUR_TIME = "(1[012]|[1-9]|0[1-9])(:|.)?" + "[0-5][0-9](\\s)?(?i)(am|pm)";
-	private static final String REGEX_24_HOUR_SIMPLE_TIME = "([01]?[0-9]|2[0-3])";
-	private static final String REGEX_24_HOUR_TIME = "([01]?[0-9]|2[0-3])(:|.)?[0-5][0-9]";
+	private static final String REGEX_TWELVE_HOUR_SIMPLE_TIME = "(1[012]|[1-9]|0[1-9])(\\s)?(?i)(am|pm)";
+	private static final String REGEX_TWELVE_HOUR_TIME = "(1[012]|[1-9]|0[1-9])(:|.)?" + 
+	                                                     "[0-5][0-9](\\s)?(?i)(am|pm)";
+	private static final String REGEX_TWENTY_FOUR_HOUR_SIMPLE_TIME = "([01]?[0-9]|2[0-3])";
+	private static final String REGEX_TWENTY_FOUR_HOUR_TIME = "([01]?[0-9]|2[0-3])(:|.)?[0-5][0-9]";
 
 	private static final String STRING_ONE_DOT = ".";
 	private static final String STRING_ONE_COLON = ":";
 	private static final String STRING_EMPTY = "";
 
+	private static final String TIME_AM = "am";
+	private static final String TIME_PM = "pm";
 	private static final String TIME_MIDNIGHT = "0000";
 	private static final String TIME_ZERO_HOUR = "00";
 	private static final String TIME_ZERO_MINUTE = "00";
+	private static final String TIME_ZERO = "0";
+	private static final int TIME_HOUR_TWELVE = 12;
 	private static final int TIME_MAXIMUM = 2400;
 	private static final int TIME_ADD_TWELVE = 12;
+	private static final int TIME_LENGTH_ONE = 1;
+	private static final int TIME_LENGTH_TWO = 2;
+	private static final int TIME_LENGTH_THREE = 3;
 
-	public TimeParser() {
-
-	}
-
+	/**
+	 * Depending on which REGEX the time String matches to, we will
+	 * call the appropriate method to do the appropriate removal of 
+	 * STRING_ONE_DOT and STRING_ONE_COLON, followed by the appropriate
+	 * padding of zeroes and accounting for the time period (am/pm) as
+	 * well as the special cases
+	 * 
+	 * @param time
+	 * @return String
+	 */
 	protected String getTime(String time) {
-		String timeString;
-		if (time.matches(REGEX_12_HOUR_SIMPLE_TIME)) {
-			String period = time.substring(time.length() - 2).toLowerCase();
-			String hourString = time.substring(0, time.length() - 2).trim();
-			int hourInt = Integer.parseInt(hourString);
-			if (period.equals("pm") && hourInt != 12) {
-				hourInt += TIME_ADD_TWELVE;
-			}
-			hourString = Integer.toString(hourInt);
-			if (period.equals("am") && hourInt == 12) {
-				timeString = TIME_MIDNIGHT;
-			} else if (hourString.length() == 1) {
-				timeString = "0" + hourString + TIME_ZERO_MINUTE;
-			} else {
-				timeString = hourString + TIME_ZERO_MINUTE;
-			}
-			return timeString;
-		} else if (time.matches(REGEX_12_HOUR_TIME)) {
-			String period = time.substring(time.length() - 2).toLowerCase();
-			timeString = time.substring(0, time.length() - 2).trim();
-			timeString = timeString.replace(STRING_ONE_DOT, STRING_EMPTY);
-			timeString = timeString.replace(STRING_ONE_COLON, STRING_EMPTY);
-			String minuteString = timeString.substring(timeString.length() - 2);
-			String hourString = timeString.substring(0, timeString.length() - 2);
-			int hourInt = Integer.parseInt(hourString);
-			if (period.equals("pm") && hourInt != 12) {
-				hourInt += TIME_ADD_TWELVE;
-			}
-			if (period.equals("am") && hourInt == 12) {
-				return TIME_ZERO_HOUR + minuteString;
-			}
-			hourString = Integer.toString(hourInt);
-			if (hourString.length() == 1) {
-				timeString = "0" + hourString + minuteString;
-			} else {
-				timeString = hourString + minuteString;
-			}
-			if (Integer.parseInt(timeString) < TIME_MAXIMUM) {
-				return timeString;
-			}
-		} else if (time.matches(REGEX_24_HOUR_SIMPLE_TIME)) {
-			timeString = time;
-			if (time.length() == 1) {
-				timeString = "0" + time + TIME_ZERO_MINUTE;
-			} else if (time.length() == 2) {
-				timeString = time + TIME_ZERO_MINUTE;
-			}
-			return timeString;
-		} else if (time.matches(REGEX_24_HOUR_TIME)) {
-			timeString = time;
-			timeString = timeString.replace(".", "");
-			timeString = timeString.replace(":", "");
-			if (timeString.length() == 3) {
-				timeString = "0" + timeString;
-			}
-			if (Integer.parseInt(timeString) < TIME_MAXIMUM) {
-				return timeString;
-			}
+		if (time.matches(REGEX_TWELVE_HOUR_SIMPLE_TIME)) {
+			return getTwelveHourSimpleTime(time);
+		} else if (time.matches(REGEX_TWELVE_HOUR_TIME)) {
+			return getTwelveHourTime(time);
+		} else if (time.matches(REGEX_TWENTY_FOUR_HOUR_SIMPLE_TIME)) {
+			return getTwentyFourHourSimpleTime(time);
+		} else if (time.matches(REGEX_TWENTY_FOUR_HOUR_TIME)) {
+			return getTwentyFourHourTime(time);
+		} else {
+			return null;
 		}
-		return null;
 	}
 
+	// To check if endTime is larger than startTime
 	protected boolean areValidTimes(String startTime, String endTime) {
 		int intStartTime = Integer.parseInt(startTime);
 		int intEndTime = Integer.parseInt(endTime);
@@ -92,6 +65,81 @@ public class TimeParser {
 			return true;
 		} else {
 			return false;
+		}
+	}
+
+	private String getTwelveHourSimpleTime(String time) {
+		String period = time.substring(time.length() - POSITION_DIFFERENCE_TWO).toLowerCase();
+		String hourString = time.substring(CommandParser.POSITION_FIRST_INDEX, 
+				                           time.length() - POSITION_DIFFERENCE_TWO).trim();
+		int hourInt = Integer.parseInt(hourString);
+		
+		if (period.equals(TIME_PM) && hourInt != TIME_HOUR_TWELVE) {
+			hourInt += TIME_ADD_TWELVE;
+		}
+		
+		hourString = Integer.toString(hourInt);
+		if (period.equals(TIME_AM) && hourInt == TIME_HOUR_TWELVE) {
+			time = TIME_MIDNIGHT;
+		} else if (hourString.length() == TIME_LENGTH_ONE) {
+			time = TIME_ZERO + hourString + TIME_ZERO_MINUTE;
+		} else {
+			time = hourString + TIME_ZERO_MINUTE;
+		}
+		return time;
+	}
+	
+	private String getTwelveHourTime(String time) {
+		String period = time.substring(time.length() - POSITION_DIFFERENCE_TWO).toLowerCase();
+		time = time.substring(CommandParser.POSITION_FIRST_INDEX, 
+				              time.length() - POSITION_DIFFERENCE_TWO).trim();
+		time = time.replace(STRING_ONE_DOT, STRING_EMPTY);
+		time = time.replace(STRING_ONE_COLON, STRING_EMPTY);
+		String minuteString = time.substring(time.length() - POSITION_DIFFERENCE_TWO);
+		String hourString = time.substring(CommandParser.POSITION_FIRST_INDEX, 
+				                           time.length() - POSITION_DIFFERENCE_TWO);
+		
+		int hourInt = Integer.parseInt(hourString);
+		if (period.equals(TIME_PM) && hourInt != TIME_HOUR_TWELVE) {
+			hourInt += TIME_ADD_TWELVE;
+		}
+		if (period.equals(TIME_AM) && hourInt == TIME_HOUR_TWELVE) {
+			return TIME_ZERO_HOUR + minuteString;
+		}
+		
+		hourString = Integer.toString(hourInt);
+		if (hourString.length() == TIME_LENGTH_ONE) {
+			time = TIME_ZERO + hourString + minuteString;
+		} else {
+			time = hourString + minuteString;
+		}
+		if (Integer.parseInt(time) < TIME_MAXIMUM) {
+			return time;
+		} else {
+			return null;
+		}
+	}
+	
+	private String getTwentyFourHourSimpleTime(String time) {
+		if (time.length() == TIME_LENGTH_ONE) {
+			time = TIME_ZERO + time + TIME_ZERO_MINUTE;
+		} else if (time.length() == TIME_LENGTH_TWO) {
+			time = time + TIME_ZERO_MINUTE;
+		}
+		return time;
+	}
+	
+	private String getTwentyFourHourTime(String time) {
+		time = time.replace(STRING_ONE_DOT, STRING_EMPTY);
+		time = time.replace(STRING_ONE_COLON, STRING_EMPTY);
+		
+		if (time.length() == TIME_LENGTH_THREE) {
+			time = TIME_ZERO + time;
+		}
+		if (Integer.parseInt(time) < TIME_MAXIMUM) {
+			return time;
+		} else {
+			return null;
 		}
 	}
 
